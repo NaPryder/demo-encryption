8174d73697f3f3cf1ee2d6b0f94b8751190b18ed
Nachai <56039177+NaPryder@users.noreply.github.com>
2024-05-04 10:22:03 +0700

    remove validator

--- src/encryptions/encryptions.controller.ts
+++ src/encryptions/encryptions.controller.ts
@@ -1,23 +1,25 @@
-import { Body, Controller, Get, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors, ValidationPipe } from '@nestjs/common';
 import { EncryptionService } from './encryptions.service'
 import { ApiTags } from '@nestjs/swagger';
 import { EncryptionDto } from './dto/encryption.dto';
-import { CommonValidationPipe } from 'src/utils/validations.util';
 import { DecryptionDto } from './dto/decryption.dto';
+import { ErrorsInterceptor, TransformationInterceptor } from 'src/encryptions/encryptions.interceptor';
 
 @ApiTags('Encryption')
+@UseInterceptors(new TransformationInterceptor())
+@UseInterceptors(new ErrorsInterceptor())
 @Controller()
 export class EncryptionController {
   constructor(private readonly appService: EncryptionService) { }
 
 
   @Post("get-encrypt-data")
-  encryption(@Body(CommonValidationPipe) encryptionBody: EncryptionDto) {
-    return this.appService.encryption(encryptionBody);
+  encryption(@Body(ValidationPipe) body: EncryptionDto) {
+    return this.appService.encryption(body);
   }
 
   @Post("get-decrypt-data")
-  decryption(@Body(CommonValidationPipe) decryptionBody: DecryptionDto) {
-    return this.appService.decryption(decryptionBody);
+  decryption(@Body(ValidationPipe) body: DecryptionDto) {
+    return this.appService.decryption(body);
   }
 }
